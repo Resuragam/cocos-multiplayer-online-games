47f8c5f1107383a1457a6311ca5f8eb3da97b28c
Resuragam <1791614500@qq.com>
2023-10-26 21:57:40 +0800

    :lock: pref: release listening events

--- assets/Scripts/UI/JoyStickManager.ts
+++ assets/Scripts/UI/JoyStickManager.ts
@@ -23,6 +23,7 @@ export class JoyStickManager extends Component {
     onDestroy() {
         input.off(Input.EventType.TOUCH_START, this.onTouchStart, this);
         input.off(Input.EventType.TOUCH_END, this.onTouchEnd, this);
+        input.off(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
     }
 
     onTouchStart(event: EventTouch) {
